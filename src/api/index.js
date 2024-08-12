db58835e3665f2cb4e8e41f6515e67edaf50d14e
zohiddev <rockrivich6@gmail.com>
2024-08-12 17:52:55 +0500

    add axios

---
+++ src/api/index.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+let Axios = axios.create({
+  baseURL: 'https://8d9d85df956fa18e.mokky.dev',
+})
+
+export default Axios
---
+++ src/constants/urls.js
@@ -0,0 +1,5 @@
+export const urls = {
+  category: {
+    get: '/category',
+  },
+}
