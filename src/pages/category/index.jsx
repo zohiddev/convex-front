9fc40280bfa878f95c97b1d898742847f5e96b12
zohiddev <rockrivich6@gmail.com>
2024-08-12 16:28:56 +0500

    change category page

--- src/pages/category/index.jsx
+++ src/pages/category/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function CategoryPage() {
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  return <div>CategoryPage</div>
+  return <div>new category page</div>
 }
 
 export default CategoryPage
